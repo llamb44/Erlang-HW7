870381515cebf27922ac9bc5e9b4bbe540e930ab
llamb44 <79153473+llamb44@users.noreply.github.com>
2022-12-20 02:14:20 -0500

    Add files via upload
    This is the final (functional) version of this project.

--- hw7_combined.erl
+++ hw7_combined.erl
@@ -1,45 +1,52 @@
+%%Lindsay Lamy, and Ian Demusis
+%%HW7
+%%c("C:\\Users\\Sweet\\OneDrive\\Documents\\College Stuff\\Actual Class Things\\fall2022\\Erlang\\hw7_combined.erl").
+
 -module(hw7_combined).
 -export([start/0, signup/2,  post/3, stop/0]).
 
 %LL: the client code
-signup(Name, Password) -> messageboard! {signup, Name, Password}.
+start() ->
+   register(messageboard, AuthPid= spawn (fun() -> auth([]) end)), register(postServer, spawn(fun() -> posting(AuthPid) end)), AuthPid.
+	
+signup(Name, Password) -> messageboard ! {self(), signup, Name, Password}.
 
-post(Name, Password, Text) -> messageboard ! {post, Name, Password, Text}.
+post(Name, Password, Text) -> messageboard ! {self(), post, Name, Password, Text}.
 
-stop() -> stop_cmd ! messageboard.
+stop() ->  messageboard ! postServer ! stop_cmd.
 
-auth(Idlist, PostServer) -> 
+auth(Idlist) ->
     receive
         {Pid, signup, Name, Password} ->
-            if 
-                keysearch(Name, 1, Idlist) -> 
-                    Pid ! {Pid, duplicate};
-            true ->
-                Cookie = erlang:md5(Name++Password),
-                UpdatedIdlist = [[Name, Cookie]|Idlist], 
-                Pid ! {Pid, ok},
-                auth(UpdatedIdlist, PostServer)
+			Cookie = erlang:md5(Name++Password),
+            %case lists:keysearch(Name, 1, Idlist) of
+			case lists:member(Cookie, Idlist) of
+                true -> Pid ! {Pid, duplicate}, auth(Idlist);
+                false ->
+                        UpdatedIdlist = [[Name, Cookie]|Idlist], 
+                        Pid ! {Pid, ok},
+                        auth(UpdatedIdlist)
             end;
 
         {Pid, post, Name, Password, Text} ->
             Cookie = erlang:md5(Name++Password),
-            if
-                keysearch(Cookie, 2, Idlist) -> 
-                    PostServer ! {Pid, post, Text}, %post server needs to be created
-                    Pid ! {Pid, ok};
-            true ->
-                Pid ! {Pid, error}
-            end,
-        auth(Idlist, PostServer)
+            %case lists:keysearch(Cookie, 2, Idlist) of
+			case lists:member([Name, Cookie], Idlist) of
+                true -> postServer ! {self(), post, Text},
+                         Pid ! {Pid, ok},
+						 auth(Idlist);
+                false -> Pid ! {Pid, error}, auth(Idlist),
+            auth(Idlist)
+            end;
+			
+        stop_cmd -> unregister(messageboard), erlang:exit("stopped")
     end.
 
-posting()->
+posting(AuthPid)->
 	receive
-		{Pid, post, Text} when Pid =:= messageboard -> io:format("~p~n", Text), posting()
+		{Pid, post, Text} when Pid =:= AuthPid -> io:format("~p~n", [Text]), posting(AuthPid);
+        stop_cmd -> unregister(postServer), erlang:exit("stopped")
 	end.
         
-start() ->
-    PostServer = spawn(fun() -> posting() end),
-    Pid = spawn(fun() -> auth([], PostServer) end),
-    register(messageboard, Pid).
 
+	
